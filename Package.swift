a12024dd24cbb2879248dbf2945f3b0c4798981c
ss-pq <ss-pq@users.noreply.github.com>
2017-05-16 20:18:28 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporMongo",
     dependencies: [
     	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-		.Package(url: "https://github.com/vapor/mongo-driver.git", majorVersion: 1)
+		.Package(url: "https://github.com/ss-pq/mongo-driver.git", majorVersion: 1)
     ]
 )
